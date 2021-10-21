dcafe8509c142bbf1a74814e52833ce594973489
Francisco Tomas Guzman Fernandez <francistomas96@gmail.com>
2021-10-21 09:16:58 +0200

    first commit

---
+++ src/main/java/com/nttdata/bootcamp/actuatorDemo/ActuatorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.actuatorDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/actuatorDemo/ActuatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.actuatorDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
